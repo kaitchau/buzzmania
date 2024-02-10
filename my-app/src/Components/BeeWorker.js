7ed22cdb457ee1e6cfca7c0b382c7a99d358178b
felicengie <felice.chandra@stonybrook.edu>
2024-02-10 17:39:41 -0500

    centered sunflower and bee

--- my-app/src/Components/BeeWorker.js
+++ my-app/src/Components/BeeWorker.js
@@ -5,13 +5,16 @@ function BeeWorker(props) {
     }
 
     return (
-    
-    <a className="flower1" onClick={handleClick} style={{border: '2px solid black', display: 'inline-block' }}>
-      <div><img src={props.imagePath} className="w-52" alt="Dynamic Flower" /></div>
-      <div>{props.points}</div>
-      <div>{props.timeout}</div>
-     </a>
-    );
+        <a className="bee-worker block border-2 border-black inline-block p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out" onClick={handleClick}>
+          <div className="mb-2">
+            <img src={props.imagePath} className="w-52 mx-auto" alt="Dynamic Bee" />
+          </div>
+          <div className="text-center">
+            <span className="block text-xl font-bold">{props.points} Points</span>
+            <span className="block text-sm">{props.timeout} Timeout</span>
+          </div>
+        </a>
+      );
   }
 
 
--- my-app/src/Components/Flower.js
+++ my-app/src/Components/Flower.js
@@ -7,14 +7,18 @@ function Flower(props) {
     }
 
     return (
-    
-    <a className="flower1" onClick={handleClick} style={{border: '2px solid black', display: 'inline-block' }}>
-      <div><img src={props.imagePath} className="w-53" alt="Dynamic Flower" /></div>
-      <div>{props.points}</div>
-      <div>{props.timeout}</div>
-     </a>
-    );
-  }
+        <a className="flower1 block border-2 border-black inline-block p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out" onClick={handleClick}>
+          <div className="mb-2">
+            <img src={props.imagePath} className="w-60 mx-auto" alt="Dynamic Flower" />
+          </div>
+          <div className="text-center">
+            <span className="block text-xl font-bold">{props.points} Points</span>
+            <span className="block text-sm">{props.timeout} Timeout</span>
+          </div>
+        </a>
+      );
+    }
+
 
   const element = <Flower image="../img/flower1.png"/>;
 
