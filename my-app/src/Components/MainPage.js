2d0b10866a8aeb77b1cbc58fb6046e79dc7a1c9f
felicengie <felice.chandra@stonybrook.edu>
2024-02-10 12:29:36 -0500

    working tailwind

---
+++ my-app/src/Components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainPage(props) {
+  return (
+    <header className="rgb(251, 191, 36)">
+      <h1 class="text-yellow-500">Buzz Mania</h1>
+    </header>
+  );
+}
+
+
+export default MainPage;
