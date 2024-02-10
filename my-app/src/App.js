df1fdb04ab6adf57fc9c0bf3ababf943221f873f
felicengie <felice.chandra@stonybrook.edu>
2024-02-10 01:26:22 -0500

    scratch

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './components/MainPage';
+
+function App() {
+  return (
+    <div className="App ">
+      <MainPage className="text-3xl font-bold underline"></MainPage>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/MainPage.js
@@ -0,0 +1,10 @@
+function MainPage(props) {
+    return <>
+    {/* header */}
+    <h1 class="">Buzz Mania</h1>
+
+    </>
+     
+  }
+
+export default MainPage;
